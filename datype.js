a7253c4fa99753c72a8e9f96d27dbe435b458e65
Ayush Kumar <143243106+Ayushtyagi3531@users.noreply.github.com>
2024-09-15 18:16:05 +0000

    datatype

--- datype.js
+++ datype.js
@@ -26,3 +26,26 @@ const myfun=function(){
     console.log('Ayush Tyagi')
 }
 console.log(typeof myfun)
+
+
+
+//for primitive datatype stack is used for memory storage
+
+//it store the copy the variable and also it change the change the copy value not change the actual value for example
+let a=12
+let b=a
+b=10
+console.log(a)
+console.log(b)
+
+
+//for non primitive datatype the heap is used for the memory storage and takes the refernce of the actual value 
+
+let my={
+    name:'Ayush Tyagi'
+}
+let my2=my
+
+my2.value='Tyagi'
+console.log(my)
+console.log(my2)
