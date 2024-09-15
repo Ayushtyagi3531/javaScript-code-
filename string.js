15c4f0df1775367eff35180c94a1a84bd6cc7676
Ayush Kumar <143243106+Ayushtyagi3531@users.noreply.github.com>
2024-09-15 19:45:22 +0000

    string methods

---
+++ string.js
@@ -0,0 +1,16 @@
+const name='Ayush'
+const surname='Tyagi'
+
+console.log(`My name is ${name} and my surname is ${surname}`)
+
+const village=String('ramnagar')
+console.log(village[0])
+console.log(village.toUpperCase())
+console.log(village.toLowerCase())
+console.log(village.charAt(4))
+console.log(village.indexOf('a'))
+console.log(village.slice(2,6))
+ const url='http://ayush.com/ayush%20tyagi'
+ 
+ console.log(url.replace('%20','-'))
+ console.log(url.split('.'))
