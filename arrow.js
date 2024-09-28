ee76b7952efdd44d93524659aee7d3fd98d868f2
Ayush Kumar <143243106+Ayushtyagi3531@users.noreply.github.com>
2024-09-28 18:21:23 +0000

    arrow

---
+++ arrow.js
@@ -0,0 +1,27 @@
+const user={
+    username:'Ayush Tyagi',
+    price:999,
+    welcomeMessage:function(){
+        console.log(`${this.username}, Welcome to our website.`)
+    }
+}
+//user.welcomeMessage()
+//user.username='Anchal'
+//user.welcomeMessage()
+
+function one(){
+    let user=14
+    console.log(user)
+    //console.log(this.user) this keyword  is giving the undefined value 
+}
+//one()
+
+//explicit return 
+const two=(num)=>{
+    const user=14
+    return user+num
+}
+two(4)
+//implicit return 
+const three=(num1,num2)=>(num1+num2)
+console.log(three(3,4))
